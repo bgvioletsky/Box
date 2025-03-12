0d5769a8d3cad198d55cc0a4e93d95deef3824d1
bgcode <bglhcode@qq.com>
2025-03-12 16:27:03 +0800

    s

---
+++ config/delete.sh
@@ -0,0 +1,18 @@
+ runs=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${{ github.repository }}/actions/workflows/${{github.workflow}}/runs" | jq -r '.workflow_runs[].id')
+          
+          # 将运行记录ID转换为数组
+          IFS=$'\n' read -r -d '' -a run_ids <<< "$runs"
+          
+          # 计算需要删除的运行记录数量
+          num_to_delete=$(( ${#run_ids[@]} - 5 ))
+          
+          # 删除超过20个的旧工作流运行记录
+          if [ $num_to_delete -gt 0 ]; then
+            for (( i=0; i<$num_to_delete; i++ )); do
+              echo "Deleting run ID: ${run_ids[$i]}"
+              curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${{ github.repository }}/actions/runs/${run_ids[$i]}"
+            done
+          else
+            echo "No runs to delete."
+          fi
+           
