203d1b81eebda34cf9f79a47b39c6b419dac1b16
bgcode <bglhcode@qq.com>
2025-03-12 16:30:46 +0800

    s

--- config/delete.sh
+++ config/delete.sh
@@ -1,4 +1,4 @@
- runs=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${{ github.repository }}/actions/workflows/${{github.workflow}}/runs" | jq -r '.workflow_runs[].id')
+ runs=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/workflows/$GITHUB_WORKFLOW/runs" | jq -r '.workflow_runs[].id')
           
           # 将运行记录ID转换为数组
           IFS=$'\n' read -r -d '' -a run_ids <<< "$runs"
@@ -10,7 +10,7 @@
           if [ $num_to_delete -gt 0 ]; then
             for (( i=0; i<$num_to_delete; i++ )); do
               echo "Deleting run ID: ${run_ids[$i]}"
-              curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/${{ github.repository }}/actions/runs/${run_ids[$i]}"
+              curl -X DELETE -H "Authorization: token $GITHUB_TOKEN" -H "Accept: application/vnd.github.v3+json" "https://api.github.com/repos/$GITHUB_REPOSITORY/actions/runs/${run_ids[$i]}"
             done
           else
             echo "No runs to delete."
